11acc5df94b867c28d8850f2e5aac8dc28bb75a9
Sonny Bennett <RC0042859@student.riversidecollege.ac.uk>
2021-05-10 14:48:37 +0100

    added label textbox

--- Forces/Form1.cs
+++ Forces/Form1.cs
@@ -19,16 +19,42 @@ namespace Forces
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            double sin(double x)
-            {
-                return (Math.Sin(x * Math.PI / 180.0));
-            }
-            double asin(double x)
-            {
-                return (Math.Asin(x) * 180 / Math.PI);
-                   
-            }
-               
+           
+
+
+        }
+        double sin(double x)
+        {
+            return (Math.Sin(x * Math.PI / 180.0));
+        }
+        double asin(double x)
+        {
+            return (Math.Asin(x) * 180 / Math.PI);
+
+        }
+        double cos(double x)
+        {
+            return (Math.Cos(x * Math.PI / 180.0));
+        }
+        double acos(double x)
+        {
+            return (Math.Acos(x) * 180 / Math.PI);
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double force = double.Parse(textBox1.Text);
+            double Angle = double.Parse(textBox2.Text);
+
+            double Fx = force * cos(Angle);
+            double Fy = force * sin(Angle);
+
+            label1.Text = "Fx = " + Fx;
+            label2.Text = "Fy = " + Fy;
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
--- Forces/Properties/Resources.Designer.cs
+++ Forces/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Forces.Properties
-{
-
-
+namespace Forces.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,64 @@ namespace Forces.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Forces.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _15042E31_DB1C_40B5_BF16_864DA8EF3581__png {
+            get {
+                object obj = ResourceManager.GetObject("_15042E31_DB1C_40B5_BF16_864DA8EF3581__png", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap visual_studio {
+            get {
+                object obj = ResourceManager.GetObject("visual_studio", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
